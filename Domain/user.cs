39e6dd40abc4afc586da3c081b7d681bf848d8aa
mika <mika@DESKTOP-G5VMJAL>
2025-07-23 11:55:10 +0300

    useradd

--- Domain/user.cs
+++ Domain/user.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace Domain
 {
-    internal class user
+    public class user : BaseDomainEntity
     {
+        Guid appId {  get; set; }
+        string userName {  get; set; }
     }
 }
