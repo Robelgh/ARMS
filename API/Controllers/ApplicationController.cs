83a96920b207724cad0429fcdf3b5634f0506b38
esaya <esaya@ESAYAS>
2025-08-25 15:01:11 +0300

    new

---
+++ API/Controllers/ApplicationController.cs
@@ -0,0 +1,46 @@
+﻿
+using API;
+using API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationRoleManagement.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApplicationController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+      {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public ApplicationController(ILogger<WeatherForecastController> logger)
+        {
+            // _logger = logger;
+        }
+
+        [HttpGet(Name = "GetaPPLICATIONS")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        public IActionResult Register(string name)
+        {
+            return Ok($"Application {name} registered successfully.");
+
+            // GET: /Application/Pending
+
+        }
+    }
+}
